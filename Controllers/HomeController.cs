14405039e6116e77e95ed56e4496d76c9e651ae8
Aryan <Aryan.saedi.1384@gmail.com>
2025-02-18 10:42:25 +0330

    added ContactUs and rtl

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,10 @@ public class HomeController : Controller
     {
         return View();
     }
-
+    public IActionResult ContactUs()
+    {
+        return View();
+    }
     public IActionResult Privacy()
     {
         return View();
